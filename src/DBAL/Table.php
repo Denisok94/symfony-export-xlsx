bce1b87e129d94bc887e280410775528fcf974a2
Denis <pdenisok@mail.ru>
2023-06-23 13:35:04 +0300

    [+] DBAL\Table

---
+++ src/DBAL/Table.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Denisok94\SymfonyExportXlsxBundle\DBAL;
+
+class Table
+{
+    public const A = 1;
+    public const B = 2;
+    public const C = 3;
+    public const D = 4;
+    public const E = 5;
+    public const F = 6;
+    public const G = 7;
+    public const H = 8;
+    public const I = 9;
+    public const J = 10;
+    public const K = 11;
+    public const L = 12;
+    public const M = 13;
+    public const N = 14;
+    public const O = 15;
+    public const P = 16;
+    public const Q = 17;
+    public const R = 18;
+    public const S = 19;
+    public const T = 20;
+    public const U = 21;
+    public const V = 22;
+    public const W = 23;
+    public const X = 24;
+    public const Y = 25;
+    public const Z = 26;
+}
